a96fed7512deb186d857e62e15bbd878464f8117
JakeXWolf <jakewolf32@gmail.com>
2019-09-26 23:18:36 -0400

    Fixed primeng to show components when ran

--- BartonApp/src/app/app.component.ts
+++ BartonApp/src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button'
+import { Component, OnInit } from "@angular/core";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
+
+export interface previousSchedule {
+  weekday;
+  weekend;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'BartonApp';
+export class AppComponent implements OnInit {
+  title = "BartonApp";
 
-  ngOnInit(): void {
-    
+  scheds: previousSchedule[];
+  cols: any[];
+  
+
+  ngOnInit() {
+    this.scheds = [
+      {weekday: "kdaja", weekend: "2018"},
+      {weekday: "dahjd", weekend: "2018"},
+      {weekday: "dfewq", weekend: "2018"}
+    ]
+
+    this.cols = [
+      { field: "weekday", header: "Weekday" },
+      { field: "weekend", header: "Weekend" }
+    ];
   }
 }
--- BartonApp/src/app/app.module.ts
+++ BartonApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
